0bb7ae2c807a330deceeed7876fe13308b44eba7
Hadar Ben Oliel <hadar.benoliel@gmail.com>
2026-06-22 00:28:48 +0300

    Fix login redirect URL and update views

--- feed/urls.py
+++ feed/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'feed'
 urlpatterns = [
     path('', views.HomePage.as_view(), name='index'),
-   
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path("new/", views.CreateNewPost.as_view(), name="new_post"),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,10 +1,26 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Post
 
+
 class HomePage(ListView):
-    http_method_names = ['get']
-    template_name = 'feed/homepage.html'
+    http_method_names = ["get"]
+    template_name = "feed/homepage.html"
     model = Post
-    context_object_name = 'posts'
+    context_object_name = "posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
 
+
+class PostDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = "feed/detail.html"
+    model = Post
+    context_object_name = "post"
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    template_name = "feed/create.html"
+    model = Post
+    context_object_name = "posts"
+    fields = ["text"]
